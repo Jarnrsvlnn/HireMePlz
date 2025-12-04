7b05a2638794fad8dda20d81cd6edb46557e8b56
Jarnrsvlnn <173128640+jarnrsvlnn@users.noreply.github.com>
2025-12-04 10:21:35 +0800

    updated the request method of update crud from PUT to PATCH

--- resources/views/components/dialog-modal.blade.php
+++ resources/views/components/dialog-modal.blade.php
@@ -11,7 +11,7 @@
 
             <form action="{{ route('jobs.update', $job) }}" method="POST" class="mt-4">
                 @csrf
-                @method('PUT')
+                @method('PATCH')
                 <div class="grid grid-cols-1 gap-6 sm:grid-cols-2">
                     {{-- JOB TITLE --}}
                     <div>
@@ -48,11 +48,11 @@
                 <div class="mt-5 flex justify-end gap-3">
                     <button id="close-dialog" type="button" formmethod="dialog"
                         class="px-4 py-2 border rounded-md text-white dark:border-gray-700">
-                        Close
+                        Cancel
                     </button>
                     <x-button buttonType='button' type="submit"
                         class="px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-500">
-                        Update
+                        Save
                     </x-button>
                 </div>
             </form>
