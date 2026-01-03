a2cacf5dc3fc4179bac69b2a49f3e3d0774ca8f7
Jarnrsvlnn <173128640+jarnrsvlnn@users.noreply.github.com>
2026-01-03 10:12:42 +0800

    implemented sorting of jobs from the most latest jobs, oldest, and by tier

--- app/Actions/Job/GetAllJobs.php
+++ app/Actions/Job/GetAllJobs.php
@@ -8,10 +8,39 @@ class GetAllJobs {
 
     public function __invoke(Request $request)
     {
-        return $request->user()
-                ->jobs()
-                ->select('jobs.id', 'jobs.job_title', 'jobs.salary', 'jobs.description', 'jobs.job_tier')
-                ->latest()
-                ->paginate(16);
+
+        $sortMethod = $request->query('sort');
+        $query = $request->user()
+                        ->jobs()
+                        ->select('jobs.id', 'jobs.job_title', 'jobs.salary', 'jobs.description', 'jobs.job_tier');
+        
+        switch($sortMethod) 
+        {
+            case 'newest':
+                $query->orderBy('created_at', 'desc');
+                break;
+            case 'oldest':
+                $query->orderBy('created_at', 'asc');
+                break;
+            case 'tier':
+                $query->orderByRaw("
+                    CASE job_tier
+                        WHEN 'Godlike' THEN 1
+                        WHEN 'Legendary' THEN 2
+                        WHEN 'Epic' THEN 3
+                        WHEN 'Kinda mid' THEN 4
+                        WHEN 'Uncommon' THEN 5
+                        WHEN 'Common' THEN 6
+                    END
+                ");
+                break;
+            default:
+                $query->latest();
+                break;
+        }
+
+        return $query
+            ->paginate(16)
+            ->withQueryString();
     }
 }
